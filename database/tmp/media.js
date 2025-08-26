f62d1b02fd50ce662cadb0f367290fca73934a2b
BLUEDEMON <bolaolat3000@gmail.com>
2025-08-26 23:08:16 +0100

    Add files via upload

--- database/tmp/media.js
+++ database/tmp/media.js
@@ -1,1 +1,8 @@
+const makiImages = [
+  'https://huggingface.co/spaces/api-ix/Base/resolve/main/maki-menu.jpg',
+  'https://huggingface.co/spaces/api-ix/Base/resolve/main/maki-menu2.jpg',
+  'https://huggingface.co/spaces/api-ix/Base/resolve/main/maki-menu3.jpg',
+  'https://huggingface.co/spaces/api-ix/Base/resolve/main/maki-menu4.jpg'
+];
 
+module.exports = { makiImages };
